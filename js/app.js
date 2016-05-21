ac19c75a394726e230f2a550a8542a9f8617132b
y-matsuda <y-matsuda@users.noreply.github.com>
2016-05-21 14:48:40 +0900

    エラーさせるための編集

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
 		this.storage = new app.Store(name);
 		this.model = new app.Model(this.storage);
 		this.template = new app.Template();
-		this.view = new app.View(this.template);
+		this.view = new app.View(this.templat
 		this.controller = new app.Controller(this.model, this.view);
 	}
 
